30537dcf2408da6312d65bd1d05dc5c55a2d34e2
Watchingboss <opil94@yandex.ru>
2019-01-27 19:18:00 +0300

    Add events

--- solutions/15_puzzle/15_puzzle.c
+++ solutions/15_puzzle/15_puzzle.c
@@ -1,51 +1,56 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 // SDL2 include headers
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
+
+#define log_er( str )                               \
+	{                                               \
+		printf( "%s: %s\n", str, SDL_GetError( ) ); \
+		return 0;                                   \
+	}
+
+#define termination_er( str )                       \
+	{                                               \
+		printf( "%s: %s\n", str, SDL_GetError( ) ); \
+		SDL_Quit( );                                \
+		exit( EXIT_FAILURE );                       \
+	}
 
 const int WIDTH  = 430;
 const int HEIGHT = 430;
 
 SDL_Window *window = 0;
 
-int initialization( ) {
-	if ( SDL_Init( SDL_INIT_VIDEO ) ) {
-		printf( "SDL_Init: %s\n", SDL_GetError( ) );
-		return 0;
-	}
+static int initialization( ) {
+	if ( SDL_Init( SDL_INIT_VIDEO ) ) log_er( "SDL_Init" );
+
 	window = SDL_CreateWindow( "15 puzzle", SDL_WINDOWPOS_CENTERED,
 	                           SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0 );
-	if ( !window ) {
-		printf( "SDL_CreateWindow: %s\n", SDL_GetError( ) );
-		return 0;
-	}
+	if ( !window ) log_er( "SDL_CreateWindow" );
+
 	int img_flags   = IMG_INIT_PNG;
 	int img_initted = IMG_Init( img_flags );
-	if ( ( img_initted & img_flags ) != img_flags ) {
-		printf( "IMG_Init: %s\n", SDL_GetError( ) );
-		return 0;
-	}
+	if ( ( img_initted & img_flags ) != img_flags ) log_er( "ING_Init" );
+
 	return 1;
 }
 
-SDL_Texture *make_texture( const char *path, SDL_Renderer *rend ) {
+static SDL_Texture *make_texture( const char *path, SDL_Renderer *rend ) {
 	SDL_Surface *surface = IMG_Load( path );
-	if ( !surface ) {
-		printf( "IMG_Load: %s\n", SDL_GetError( ) );
-		return 0;
-	}
+	if ( !surface ) log_er( "IMG_Load" );
+
 	SDL_Texture *texture = SDL_CreateTextureFromSurface( rend, surface );
 	SDL_FreeSurface( surface );
-	if ( !texture ) {
-		printf( "SDL_CreateTextureFromSurface: %s\n", SDL_GetError( ) );
-		return 0;
-	}
+	if ( !texture ) log_er( "SDL_CreateTextureFromSurface" );
+
 	return texture;
 }
 
-void render_texture( SDL_Texture *texture, SDL_Renderer *rend, int x, int y ) {
+static void render_texture( SDL_Texture *texture, SDL_Renderer *rend, int x, int y ) {
 	SDL_Rect dest;
 	dest.x = x;
 	dest.y = y;
@@ -54,30 +59,39 @@ void render_texture( SDL_Texture *texture, SDL_Renderer *rend, int x, int y ) {
 }
 
 int main( int argc, char *argv[] ) {
-	if ( !initialization( ) ) {
-		printf( "Initialization error\n" );
-		SDL_Quit( );
-	}
+	if ( !initialization( ) ) termination_er( "Initialization error" );
 
 	SDL_Renderer *rend = SDL_CreateRenderer(
 	    window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
 	if ( !rend ) {
 		SDL_DestroyWindow( window );
-		printf( "SDL_CreateRenderer: %s\n", SDL_GetError( ) );
-		SDL_Quit( );
-		exit( EXIT_FAILURE );
+		termination_er( "SDL_CreateRenderer" );
 	}
 
 	SDL_Texture *puzzle_grid = make_texture( "./play_grid.png", rend );
 
-	for ( int i = 0; i < 5; ++i ) {
+	SDL_Event e;
+	bool      play_game = true;
+	while ( play_game ) {
+		while ( SDL_PollEvent( &e ) ) {
+			if ( e.type == SDL_QUIT )
+				play_game = false;
+			else if ( e.type == SDL_KEYDOWN ) {
+				switch ( e.key.keysym.sym ) {
+					case SDLK_ESCAPE: play_game = false; break;
+					case SDLK_UP: puts( "UP" ); break;
+					case SDLK_DOWN: puts( "DOWN" ); break;
+					case SDLK_LEFT: puts( "LEFT" ); break;
+					case SDLK_RIGHT: puts( "RIGHT" ); break;
+				}
+			}
+		}
+
 		SDL_RenderClear( rend );
 
 		render_texture( puzzle_grid, rend, 0, 0 );
 
 		SDL_RenderPresent( rend );
-
-		SDL_Delay( 1000 );
 	}
 
 	SDL_DestroyTexture( puzzle_grid );
